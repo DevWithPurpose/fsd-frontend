cd62b820c55c8dc591f62851c660eb982eee8036
YounQsheyex <younqsheyex@gmail.com>
2026-04-02 10:02:13 -0600

    feat:added packages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Welcome from "./components/welocme";
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
