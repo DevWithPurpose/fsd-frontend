07c5c7f9750fec70530106817b37ab4937b7c1a2
holuwatobi harbolanle <bolanleoluwatobi844@gmail.com>
2026-04-10 16:10:44 +0100

    Updated the form status and some classes

--- src/components/Group 3/Page.jsx
+++ src/components/Group 3/Page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import closeIcon from "../../assets/close-icon.svg";
 import requestIcon from "../../assets/request-icon.svg";
 import offerIcon from "../../assets/offer-icon.svg";
@@ -17,8 +18,10 @@ function Guidelines({text}) {
 }
 
 const Page = () => {
+  const [desc, setDesc] = useState("");
+  const [selectedOption, setSelectedOption] = useState("request");
   return (
-    <div className="font-[Manrope] bg-[#F6FAF8] h-full pb-32.5">
+    <div className="bg-[#F6FAF8] h-full pb-32.5">
       <header className="flex items-center justify-between p-6">
         <div className="flex items-center gap-5">
           <img src={closeIcon} alt="A close icon" className="w-3.5 h-3.5" />
@@ -40,82 +43,117 @@ const Page = () => {
             community.
           </span>
         </h1>
-        <p className="mt-4 tetx-[#56615F] text-lg w-142">
+        <p className="mt-4 text-[#56615F] text-lg w-142">
           Whether you're offering a hand or seeking support, your contribution
           strengthens the digital commons. Keep it clear, keep it kind.
         </p>
 
-        <div className="mt-12 text-[#56615F] flex -center gap-12">
+        <div className="mt-12 text-[#56615F] flex items-center gap-12">
           <div>
-            <h5 className="text-sm">POST INTENT</h5>
-            <div className="flex items-center mt-4.25 gap-4">
-              <button className="bg-[#A8F0E7] border border-[#1D6A63] rounded-xl p-5 w-[266.67px] text-left transition-transform ease-in-out hover:scale-95">
-                <img src={requestIcon} alt="Request" />
-                <p className="text-[#2A3433] text-base mt-2 leading-6">
-                  Request
-                </p>
-                <p className="text-[12px] leading-4">I need some help</p>
-              </button>
-              <button className="border border-[#A9B4B24D] rounded-xl p-5 w-[266.67px] text-left transition-transform ease-in-out hover:scale-95">
-                <img src={offerIcon} alt="Offer" />
-                <p className="text-[#2A3433] text-base mt-2 leading-6">Offer</p>
-                <p className="text-[12px] leading-4">I'm lending a hand</p>
-              </button>
-            </div>
+            <form
+              method="post"
+              id="post-form"
+              className="text-sm"
+            >
+              <h5 className="text-sm">POST INTENT</h5>
+              <div className="flex items-center mt-4.25 gap-4">
+                <button
+                  className={`border rounded-xl p-5 w-[266.67px] text-left transition-transform ease-in-out hover:scale-95 ${selectedOption === "request" ? "bg-[#A8F0E7] border-[#1D6A63]" : "bg-transparent border-[#A9B4B24D]"}`}
+                  onClick={() => setSelectedOption("request")}
+                >
+                  <img src={requestIcon} alt="Request" />
+                  <p className="text-[#2A3433] text-base mt-2 leading-6">
+                    Request
+                  </p>
+                  <p className="text-[12px] leading-4">I need some help</p>
+                </button>
 
-            <form method="post" id="post-form" className="mt-10 text-sm flex flex-col gap-10">
-              <div>
-                <label htmlFor="title">TITLE</label>
-                <input
-                  type="text"
-                  name="title"
-                  id="title"
-                  placeholder="What's happening?"
-                  className="border border-[#A9B4B233] bg-[#EEF5F3] p-4 placeholder:text-[#A9B4B299] w-full rounded-t-lg outline-[#A9B4B233] text-base mt-2"
-                />
-              </div>
-              <div className="flex items-center gap-6">
-                <div>
-                  <label htmlFor="category">CATEGORY</label>
-                  <select
-                    name="category"
-                    id="category"
-                    className="bg-[#EEF5F3] rounded-t-lg border border-[#A9B4B233] w-full p-4 outline-[#A9B4B233] text-base mt-2"
+                <button
+                  className={`border rounded-xl p-5 w-[266.67px] text-left transition-transform ease-in-out hover:scale-95 ${selectedOption === "offer" ? "bg-[#A8F0E7] border-[#1D6A63]" : "bg-transparent border-[#A9B4B24D]"}`}
+                  onClick={() => setSelectedOption("offer")}
                   >
-                    <option value="household help">Household Help</option>
-                  </select>
-                </div>
+                  <img src={offerIcon} alt="Offer" />
+                  <p className="text-[#2A3433] text-base mt-2 leading-6">Offer</p>
+                  <p className="text-[12px] leading-4">I'm lending a hand</p>
+                </button>
+              </div>
 
-                <div className="flex flex-col">
-                  <label htmlFor="location">LOCATION</label>
-                  <div className="relative mt-2">
-                    <img
-                      src={location}
-                      alt="Location"
-                      className="absolute top-1/2 left-5 -translate-y-1/2"
-                    />
-                    <input
-                      type="search"
-                      name="location"
-                      id="location"
-                      placeholder="e.g. North Brooklyn"
-                      className="w-full bg-[#EEF5F3] border border-[#A9B4B233] rounded-t-lg py-4 pl-12.25 outline-[#A9B4B233] text-base"
-                    />
+              {/* {selectedOption === "request" && (
+                <div>
+                </div>
+              )}
+              {selectedOption === "offer" && (
+                <div></div>
+              )} */}
+              
+              <div className="flex flex-col gap-10 mt-10">
+                <div>
+                  <label htmlFor="title">TITLE</label>
+                  <input
+                    type="text"
+                    name="title"
+                    id="title"
+                    placeholder="What's happening?"
+                    className="border border-[#A9B4B233] bg-[#EEF5F3] p-4 placeholder:text-[#A9B4B299] w-full rounded-t-lg outline-[#A9B4B233] text-base mt-2"
+                    required
+                    maxLength={75}
+                  />
+                </div>
+                <div className="flex items-center gap-6">
+                  <div>
+                    <label htmlFor="category">CATEGORY</label>
+                    <select
+                      name="category"
+                      id="category"
+                      className="bg-[#EEF5F3] rounded-t-lg border border-[#A9B4B233] w-full p-4 outline-[#A9B4B233] text-base mt-2"
+                      required
+                    >
+                      <option value="household help">Household Help</option>
+                    </select>
+                  </div>
+                  <div className="flex flex-col">
+                    <label htmlFor="location">LOCATION</label>
+                    <div className="relative mt-2">
+                      <img
+                        src={location}
+                        alt="Location"
+                        className="absolute top-1/2 left-5 -translate-y-1/2"
+                      />
+                      <input
+                        type="search"
+                        name="location"
+                        id="location"
+                        placeholder="e.g. North Brooklyn"
+                        className="w-full bg-[#EEF5F3] border border-[#A9B4B233] rounded-t-lg py-4 pl-12.25 outline-[#A9B4B233] text-base"
+                        required
+                        minLength={5}
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div>
-                <label htmlFor="description">DESCRIPTION</label>
-                <textarea
-                  name="description"
-                  id="description"
-                  placeholder="Provide more details for your neighbors..."
-                  className="border border-[#A9B4B233] bg-[#EEF5F3] p-4 placeholder:text-[#A9B4B299] w-full rounded-t-lg outline-[#A9B4B233] text-base mt-2 h-[209.25px]"
-                ></textarea>
+                <div>
+                  <label htmlFor="description">DESCRIPTION</label>
+                  <textarea
+                    name="description"
+                    id="description"
+                    placeholder="Provide more details for your neighbors..."
+                    value={desc}
+                    onChange={(e) => setDesc(e.target.value)}
+                    className="border border-[#A9B4B233] bg-[#EEF5F3] p-4 placeholder:text-[#A9B4B299] w-full rounded-t-lg outline-[#A9B4B233] text-base mt-2 h-[209.25px] resize-none scroll"
+                    required
+                    maxLength={500}
+                  ></textarea>
+                  <p
+                    className={`text-xs text-right ${desc.length >= 450 ? "text-red-500" : "text-gray-500"}`}
+                  >
+                    {500 - desc.length} characters remaining
+                  </p>
+                </div>
               </div>
 
               <button
-                type="submit" form="post-form"
+                type="submit"
+                form="post-form"
                 className="text-[#E2FFFA] font-bold text-lg mt-16 bg-linear-[#1D6A63] w-[202.59px] py-5 rounded-full cursor-pointer shadow-[0_4_6_-4_#1D6A631A,0_10_15_-3_#1D6A631A] transition-transform ease-in-out hover:scale-95"
               >
                 Submit Post
